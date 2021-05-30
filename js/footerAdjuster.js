7a9b98c9a3a0a75586840839630e2572f6041f00
ShadyBarrios <scottpsn16@gmail.com>
2021-05-30 04:08:56 -0700

    tiny js bug

--- js/footerAdjuster.js
+++ js/footerAdjuster.js
@@ -1,5 +1,4 @@
 let deviceWidth = screen.width;
-deviceWidth = 660;
 console.log(deviceWidth);
 let bigEnough = "<td style=\"padding-left:1rem;\"> <address\> <p>Location: Bikini Bottom, Pacific Ocean</p> <p>Address: 831 Bottom Feeder Lane</p><p>Contact Us: <a href=\"mailto: scottgb04@outlook.com\">krustykrab@krustykrab.com</a></p></address></td>"
 let small = "<address><p>Location: Bikini Bottom, Pacific Ocean</p><p>Address: 831 Bottom Feeder Lane</p><p>Contact Us: <a href=\"mailto: scottgb04@outlook.com\">krustykrab@krustykrab.com</a></p></address></td>"
