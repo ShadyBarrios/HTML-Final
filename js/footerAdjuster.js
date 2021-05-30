6735acc895b3729f13c5e2a642493edeb604f3ad
ShadyBarrios <scottpsn16@gmail.com>
2021-05-30 04:45:51 -0700

    upgrades people, upgrades

--- js/footerAdjuster.js
+++ js/footerAdjuster.js
@@ -1,8 +1,15 @@
-let deviceWidth = screen.width;
-console.log(deviceWidth);
-let bigEnough = "<td style=\"padding-left:1rem;\"> <address\> <p>Location: Bikini Bottom, Pacific Ocean</p> <p>Address: 831 Bottom Feeder Lane</p><p>Contact Us: <a href=\"mailto: scottgb04@outlook.com\">krustykrab@krustykrab.com</a></p></address></td>"
-let small = "<address><p>Location: Bikini Bottom, Pacific Ocean</p><p>Address: 831 Bottom Feeder Lane</p><p>Contact Us: <a href=\"mailto: scottgb04@outlook.com\">krustykrab@krustykrab.com</a></p></address></td>"
-if(deviceWidth < 600)
-    document.write(small);
-else
-    document.write(bigEnough);
+let bigEnough = "<td style=\"padding-left:1rem;\"> <address\> <p>Location: Bikini Bottom, Pacific Ocean</p> <p>Address: 831 Bottom Feeder Lane</p><p>Contact Us: <a href=\"mailto: scottgb04@outlook.com\">krustykrab@krustykrab.com</a></p></address></td>";
+let small = "<address><p><br>Location: Bikini Bottom, Pacific Ocean</p><p>Address: 831 Bottom Feeder Lane</p><p>Contact Us: <a href=\"mailto: scottgb04@outlook.com\">krustykrab@krustykrab.com</a></p></address></td>";
+
+function footerAdjuster(){
+    let deviceWidth = window.innerWidth;
+
+    console.log(deviceWidth);
+
+    if(deviceWidth < 600)
+        document.write(small);
+    else
+        document.write(bigEnough);
+}
+
+footerAdjuster();
