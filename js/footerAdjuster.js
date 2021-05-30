966db345fbc8f7797cb344df87fff326c31e2d6e
ShadyBarrios <scottpsn16@gmail.com>
2021-05-30 04:07:40 -0700

    added the footer with js capability to enhance the experience

---
+++ js/footerAdjuster.js
@@ -0,0 +1,9 @@
+let deviceWidth = screen.width;
+deviceWidth = 660;
+console.log(deviceWidth);
+let bigEnough = "<td style=\"padding-left:1rem;\"> <address\> <p>Location: Bikini Bottom, Pacific Ocean</p> <p>Address: 831 Bottom Feeder Lane</p><p>Contact Us: <a href=\"mailto: scottgb04@outlook.com\">krustykrab@krustykrab.com</a></p></address></td>"
+let small = "<address><p>Location: Bikini Bottom, Pacific Ocean</p><p>Address: 831 Bottom Feeder Lane</p><p>Contact Us: <a href=\"mailto: scottgb04@outlook.com\">krustykrab@krustykrab.com</a></p></address></td>"
+if(deviceWidth < 600)
+    document.write(small);
+else
+    document.write(bigEnough);
