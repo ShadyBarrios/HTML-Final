e459211994a3bd452564e966e1f0e111df580b0e
ShadyBarrios <scottpsn16@gmail.com>
2021-05-30 21:17:45 -0700

    Fixed menu and added some extra details

--- js/footerAdjuster.js
+++ js/footerAdjuster.js
@@ -1,15 +1,24 @@
-let bigEnough = "<td style=\"padding-left:1rem;\"> <address\> <p>Location: Bikini Bottom, Pacific Ocean</p> <p>Address: 831 Bottom Feeder Lane</p><p>Contact Us: <a href=\"mailto: scottgb04@outlook.com\">krustykrab@krustykrab.com</a></p></address></td>";
+let bigEnough = "<td style=\"padding-left:1rem;\"> <address> <p>Location: Bikini Bottom, Pacific Ocean</p> <p>Address: 831 Bottom Feeder Lane</p><p>Contact Us: <a href=\"mailto: scottgb04@outlook.com\">krustykrab@krustykrab.com</a></p></address></td>";
 let small = "<address><p><br>Location: Bikini Bottom, Pacific Ocean</p><p>Address: 831 Bottom Feeder Lane</p><p>Contact Us: <a href=\"mailto: scottgb04@outlook.com\">krustykrab@krustykrab.com</a></p></address></td>";
+const firstPart = "<td style=\"padding-right:1rem;\"><p>The Krusty Krab&trade; </p><p> Home of the <span class=\"italic\">Krabby Patty</span>&trade;</p><p>Come spend <span class=\"italic\">your</span> money here!&trade;</p></td>"
 
 function footerAdjuster(){
     let deviceWidth = window.innerWidth;
+    let area = document.getElementById("footerTable");
 
-    console.log(deviceWidth);
-
-    if(deviceWidth < 600)
-        document.write(small);
-    else
-        document.write(bigEnough);
+    if(deviceWidth < 600){
+        let total = firstPart + small;
+        console.log("Small Display footer");
+        area.innerHTML = total;
+    }
+    else{
+        let total = firstPart + bigEnough;
+        console.log("Big Display footer");
+        area.innerHTML = total;
+    }
 }
 
+// for when the page is initially opened
 footerAdjuster();
+
+window.onresize = footerAdjuster;
---
+++ js/listExposer.js
@@ -0,0 +1,54 @@
+let foodContent = "<ul><li><a href=\"detailedMenu.html\">Detailed Menu</a></li><li><a href=\"order.html\">Order Food</a></li><li><a href=\"cart.html\">View Cart</a></li></ul>";
+let foodButton = document.getElementById("foodButton");
+let foodArea = document.getElementById("foodContent");
+let foodExpanded = false; // false = not shown. true = shown
+
+let jobsContent = "<ul><li><a href = \"hired.html\">The Hired</a></li><li><a href = \"toBeHired.html\">Job Seekers</a></li></ul>"
+let jobsButton = document.getElementById("jobsButton");
+let jobsArea = document.getElementById("jobsContent");
+let jobsExpanded = false;
+
+function procedure(innerHTML){
+    console.log("The innerHTML pass is " + innerHTML);
+    if(innerHTML == "Food"){
+        if(foodExpanded == true){
+            console.log("removing content");
+            foodArea.innerHTML = "";
+            foodExpanded = false;
+            console.log(foodExpanded);
+        }
+        else{
+            console.log("adding in content");
+            foodArea.innerHTML = foodContent;
+            foodExpanded = true;
+            console.log(foodExpanded);
+        }  
+    }
+    else if (innerHTML == "Jobs"){ // innerHTML == jobs
+        if(jobsExpanded == true){
+            console.log("removing content");
+            jobsArea.innerHTML = "";
+            jobsExpanded = false;
+            console.log(jobsExpanded);
+        }
+        else{
+            console.log("adding in content");
+            jobsArea.innerHTML = jobsContent;
+            jobsExpanded = true;
+            console.log(jobsExpanded);
+        }         
+    }
+    else    
+        console.log("Invalid innerHTML");
+}
+
+function jobsProcedure(){
+    procedure(jobsButton.innerHTML);
+}
+
+function foodProcedure(){
+    procedure(foodButton.innerHTML);
+}
+
+foodButton.addEventListener('click', foodProcedure);
+jobsButton.addEventListener('click', jobsProcedure);
