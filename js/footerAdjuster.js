289e3cd8f04fecb8223b49c149de22d34a1b1e69
ShadyBarrios <scottpsn16@gmail.com>
2021-06-06 15:29:02 -0700

    Work in progress

--- js/footerAdjuster.js
+++ js/footerAdjuster.js
@@ -1,12 +1,12 @@
 let bigEnough = "<td style=\"padding-left:1rem;\"> <address> <p>Location: Bikini Bottom, Pacific Ocean</p> <p>Address: 831 Bottom Feeder Lane</p><p>Contact Us: <a href=\"mailto: scottgb04@outlook.com\">krustykrab@krustykrab.com</a></p></address></td>";
 let small = "<address><p><br>Location: Bikini Bottom, Pacific Ocean</p><p>Address: 831 Bottom Feeder Lane</p><p>Contact Us: <a href=\"mailto: scottgb04@outlook.com\">krustykrab@krustykrab.com</a></p></address></td>";
-const firstPart = "<td style=\"padding-right:1rem;\"><p>The Krusty Krab&trade; </p><p> Home of the <span class=\"italic\">Krabby Patty</span>&trade;</p><p>Come spend <span class=\"italic\">your</span> money here!&trade;</p></td>"
+const firstPart = "<td style=\"padding-right:1rem;\"><p>The Krusty Krab&trade; </p><p> Home of the <span class=\"italic\">Krabby Patty</span>&trade;</p><p>Managed by Mr. Krabs</p></td>"
 
 function footerAdjuster(){
     let deviceWidth = window.innerWidth;
     let area = document.getElementById("footerTable");
 
-    if(deviceWidth < 600){
+    if(deviceWidth < 901){
         let total = firstPart + small;
         console.log("Small Display footer");
         area.innerHTML = total;
